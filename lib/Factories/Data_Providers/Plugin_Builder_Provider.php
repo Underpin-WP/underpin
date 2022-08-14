3475c4dcea6aa692f70b46ef1b3c584b36a7b382
Alex Standiford <a@alexstandiford.com>
2022-08-14 17:46:46 -0400

    WIP

---
+++ lib/Factories/Data_Providers/Plugin_Builder_Provider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Underpin\Factories\Data_Providers;
+
+
+use Underpin\Interfaces\Data_Provider;
+
+class Plugin_Builder_Provider implements Data_Provider {
+
+	public function __construct( protected ServiceProvider $builder ) {
+	}
+
+	public function get_builder(): ServiceProvider {
+		return $this->builder;
+	}
+
+}
--- tests/Unit/Factories/Data_Providers/Plugin_Provider_Test.php
+++ tests/Unit/Factories/Data_Providers/Plugin_Provider_Test.php
@@ -7,7 +7,7 @@ use Generator;
 use Underpin\Factories\Data_Providers\Plugin_Provider;
 use Underpin\Tests\Test_Case;
 use Underpin\Tests\Traits\With_Getter_Tests;
-use Underpin\WordPress\Interfaces\Base;
+use Underpin\Interfaces\Base;
 
 class Plugin_Provider_Test extends Test_Case {
 
