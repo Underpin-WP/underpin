a7dfaa513588c3182da8f72cd266af175f2f67a5
Alex Standiford <a@alexstandiford.com>
2022-08-16 06:36:10 -0400

    WIP

--- lib/Interfaces/Model.php
+++ lib/Interfaces/Model.php
@@ -4,7 +4,9 @@ namespace Underpin\Interfaces;
 
 use Underpin\Exceptions\Operation_Failed;
 
-interface Model extends Identifiable {
+interface Model {
+
+	public function get_id(): string|int|null;
 
 	public function set_id( string|int $id ): static;
 
