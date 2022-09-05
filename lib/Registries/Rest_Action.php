d6971c375c21134a60cea10eb0c80188e69f34f0
Alex Standiford <a@alexstandiford.com>
2022-09-05 13:22:23 -0400

    WIP

--- lib/Registries/Rest_Action.php
+++ lib/Registries/Rest_Action.php
@@ -3,18 +3,21 @@
 namespace Underpin\Registries;
 
 
-
+use Underpin\Abstracts\Rest_Middleware;
+use Underpin\Factories\Request;
+use Underpin\Helpers\Array_Helper;
 use Underpin\Interfaces\Feature_Extension;
 use Underpin\Interfaces\Has_Response;
 use Underpin\Interfaces\With_Middleware;
 
-abstract class Rest_Action implements Feature_Extension, With_Middleware {
+abstract class Rest_Action implements Feature_Extension, With_Middleware, Has_Response {
 
 	protected Object_Registry $middleware;
+	protected                 $response;
 	private bool              $middleware_ran = false;
 	protected Request         $request;
 
-	public function __construct( protected Has_Response $response ) {
+	public function __construct() {
 		$this->middleware = new Object_Registry( Rest_Middleware::class, Rest_Middleware::class );
 	}
 
@@ -24,16 +27,42 @@ abstract class Rest_Action implements Feature_Extension, With_Middleware {
 		return $this;
 	}
 
+	/**
+	 * Does the middleware actions for this request.
+	 *
+	 * @return void
+	 * @throws \Underpin\Exceptions\Middleware_Exception
+	 */
 	public function do_middleware_actions(): void {
-		Array_Helper::each( $this->middleware->to_array(), fn ( Rest_Middleware $middleware ) => $middleware->run( $this->request ) );
-		$this->middleware_ran = true;
+		if ( ! $this->middleware_ran() ) {
+			Array_Helper::each( $this->middleware->to_array(), fn ( Rest_Middleware $middleware ) => $middleware->run( $this->request ) );
+			$this->middleware_ran = true;
+		}
 	}
 
+	/**
+	 * Returns true if the middleware ran.
+	 *
+	 * @return bool
+	 */
 	public function middleware_ran(): bool {
 		return $this->middleware_ran;
 	}
 
-	public function get_response(): Has_Response {
+	/**
+	 * Set the response.
+	 *
+	 * @param mixed $value
+	 *
+	 * @return $this
+	 */
+	protected function set_response( mixed $value ): static {
+		$this->response = $value;
+
+		return $this;
+	}
+
+	public function get_response(): mixed {
 		return $this->response;
 	}
 
